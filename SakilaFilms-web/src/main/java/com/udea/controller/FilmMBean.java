57e0bec515204d7f0edb4d90d30460761fb0e0ba
ASANTAANA <ASANTAANA@DESKTOP-4OKL3ES>
2017-03-30 23:07:26 -0500

    Se crea el face-config.xml y en FilmMBean.java

---
+++ SakilaFilms-web/src/main/java/com/udea/controller/FilmMBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.controller;
+
+/**
+ *
+ * @author ASANTAANA
+ */
+public class FilmMBean {
+
+    /**
+     * Creates a new instance of FilmMBean
+     */
+    public FilmMBean() {
+    }
+    
+}
